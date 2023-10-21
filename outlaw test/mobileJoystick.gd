a7601de770f6286e933dec54c6d0c14c2593a1ed
PatrickLBrock <patrickleonard0312@gmail.com>
2023-10-20 23:19:49 -0400

    mobile buttons update
    -updated movement button to stop on release
    -inserted assets for camera move button

--- outlaw test/mobileJoystick.gd
+++ outlaw test/mobileJoystick.gd
@@ -4,11 +4,14 @@ signal button_move_player
 
 func _input(event):
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if $TouchScreenButton.is_pressed():
+		if $MoveButton.is_pressed():
 			var move_vector = calculate_move_vector(event.position)
+			emit_signal("button_move_player", move_vector)
+		else:
+			var move_vector = Vector3.ZERO
 			emit_signal("button_move_player", move_vector)
 
 func calculate_move_vector(event_position):
-	var button_radius = $TouchScreenButton.shape.get_radius()
-	var button_center = $TouchScreenButton.position + Vector2(button_radius, button_radius)
+	var button_radius = $MoveButton.shape.get_radius()
+	var button_center = $MoveButton.position + Vector2(button_radius, button_radius)
 	return (event_position - button_center).normalized()
